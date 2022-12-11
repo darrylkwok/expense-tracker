8373266c71a0e59c25a585a5b8ea24017015488c
darrylkwok <kwokdarryl@gmail.com>
2022-12-11 10:09:13 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,26 @@
+class Expense():
+   day = 1
+   month = 11
+   budget = 500
+   category = "Food"
+   description = "Macs"
+
+dayExp = Expense()
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
